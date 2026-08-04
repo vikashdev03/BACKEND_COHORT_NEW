bc80a3c8370832043a50b654939ee0588c874105
Vikash Kumar <vikashdeveloper79@gmail.com>
2026-08-04 16:16:29 +0530

    feat: initialize day-13 project with Express, MongoDB, and JWT authentication
    - created package.json with necessary dependencies
    - added server.js to set up the Express server
    - implemented app.js to configure middleware and routes
    - established database connection in database.js
    - defined user model in user.model.js
    - created auth.routes.js for user registration, login, and protected route handling

---
+++ day-13/src/models/user.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    name: String,
+    email:{
+        type : String,
+        unique: [true , "with this email user Account Alreaddy exists"]
+    },
+    password: String,
+})
+
+const userModel = mongoose.model("Users" , UserSchema)
+
+module.exports = userModel
---
+++ day-13/src/routes/auth.routes.js
@@ -0,0 +1,115 @@
+const express = require('express')
+const authRouter = express.Router()
+const jwt = require("jsonwebtoken")
+
+const userModel = require("../models/user.model")
+
+
+/* 
+* /api/auth/register
+*/
+authRouter.post("/register", async (req, res) => {
+
+    const { email, name, password } = req.body
+
+    const isUserAlreadyExists = await userModel.findOne({ email })
+
+    if (isUserAlreadyExists) {
+        return res.status(400).json({
+            message: " user already exists with this Email id"
+        })
+    }
+
+
+    const user = await userModel.create({
+        email, password, name
+    })
+
+    const token = jwt.sign(
+        {
+            id: user._id,
+            email: user.email,
+            password:user.password
+        },
+        process.env.JWT_SECRET
+    )
+
+    res.cookie("jwt_token", token)
+
+    res.status(201).json({
+        message: "user registered",
+        user,
+        token
+    })
+
+
+})
+
+/* 
+/api/auth/protected
+ */
+authRouter.post("/protected", (req, res) => {
+    console.log(req.cookies);
+
+    res.status(200).json({
+        message: "this is a protected Route"
+    })
+})
+
+
+/* 
+/api/auth/login    ---------- // controller=  jab api hit hoga to ye function call hoga 
+ */
+authRouter.post("/login", async (req, res) => {
+
+    const { email, password } = req.body
+
+    const user = await userModel.findOne({ email })
+
+    if (!user) {
+        return res.status(404).json({
+            message: "User  not found with This Email address"
+
+        })
+    }
+
+    const isPassWordMatched = user.password === password
+
+    if (!isPassWordMatched) {
+        return res.status(401).json({
+            message: "invalid Password"
+        })
+    }
+
+
+    const token = jwt.sign({
+            id: user._id,
+        },process.env.JWT_SECRET)
+
+        res.cookie("jwt_token", token) // ye token ko set karte hai res.cookie ke andar  
+
+        res.status(200).json({
+            message:"user Logged In Sucessfully",
+            user,
+            
+        })
+    
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = authRouter
