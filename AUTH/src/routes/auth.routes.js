ed546b1ee172548cb150effc002fa6066f845fc4
Vikash Kumar <vikashdeveloper79@gmail.com>
2026-08-05 22:59:57 +0530

    .

--- AUTH/src/routes/auth.routes.js
+++ AUTH/src/routes/auth.routes.js
@@ -27,10 +27,10 @@ authRouter.post('/register', async (req, res) => {
 
     const token = jwt.sign({
         id: user._id,
-    }, process.env.JWT_SECRET, {expiresIn: '1h'})
+    }, process.env.JWT_SECRET, { expiresIn: '1h' })
 
 
-    res.cookie('token' , token)
+    res.cookie('token', token)
 
     res.status(201).json({
         message: "User created successfully"
@@ -40,7 +40,52 @@ authRouter.post('/register', async (req, res) => {
 
 })
 
+// ye get-me api karti ye hai ki token deckti hai  
+authRouter.get('/get-me', async (req, res) => {
 
+    const token = req.cookies.token;
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+    const user = await userModel.findById(decoded.id)
+
+    res.json({
+        name: user.name,
+        email: user.email,
+    })
+
+}) 
+
+// for new token need to login again beacuse token is expired after 1 hour
+
+authRouter.post('/login', async (req, res) => {
+
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    const isPasswordValid = crypto.createHash("md5").update(password).digest("hex") === user.password;
+
+    if (!isPasswordValid) {
+        return res.status(401).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign({
+        id: user._id,
+    }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+    res.cookie('token', token);
+
+    res.json({
+        message: "Login successful",
+        token
+    });
+
+});
 
 module.exports = authRouter;
 
