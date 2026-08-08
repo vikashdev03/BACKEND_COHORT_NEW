2898fd874312a881decc1d6d7a55d159724bf22d
Vikash Kumar <vikashdeveloper79@gmail.com>
2026-08-08 12:30:39 +0530

    auth upadte

--- day-14/src/controllers/auth.controller.js
+++ day-14/src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const userModel = require('../models/user.model')
-const crypto = require('crypto')
+// const crypto = require('crypto')
+const bcrypt = require('bcryptjs')
 const jwt = require("jsonwebtoken")
 
 
@@ -20,8 +21,9 @@ async function registerController(req, res) {
             })
     }
 
-    const hash = crypto.createHash('sha256').update(password).digest('hex')
-
+    // const hash = crypto.createHash('sha256').update(password).digest('hex')
+        const hash =  await bcrypt.hash(password,10 )
+    
     const user = await userModel.create({
         username,
         email,
@@ -86,9 +88,10 @@ async function loginController(req, res) {
         })
     }
 
-    const hash = crypto.createHash('sha256').update(password).digest('hex')
+    // const hash = crypto.createHash('sha256').update(password).digest('hex')
+        const hash =  await bcrypt.hash(password,10 )
 
-    const isPasswordValid = hash == user.password
+    const isPasswordValid = await bcrypt.compare(password, user.password)
 
     if (!isPasswordValid) {
         return res.status(401).json({
